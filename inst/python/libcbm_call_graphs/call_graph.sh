c31b6539c4dffe19400d19d7d61ad8800eb9690e
Alex Chubaty <achubaty@for-cast.ca>
2023-03-03 15:20:27 -0700

    libcbm_model subgraphs

--- inst/python/libcbm_call_graphs/call_graph.sh
+++ inst/python/libcbm_call_graphs/call_graph.sh
@@ -6,7 +6,11 @@
 # pip3 install code2flow
 # code2flow --help
 
-code2flow ~/GitHub/PredictiveEcology/libcbmr/inst/python/libcbm/libcbm/input --language py --output inst/python/libcbm_input.png
-code2flow ~/GitHub/PredictiveEcology/libcbmr/inst/python/libcbm/libcbm/model --language py --output inst/python/libcbm_model.png
-code2flow ~/GitHub/PredictiveEcology/libcbmr/inst/python/libcbm/libcbm/storage --language py --output inst/python/libcbm_storage.png
-#code2flow ~/GitHub/PredictiveEcology/libcbmr/inst/python/libcbm/libcbm/wrapper --language py --output inst/python/libcbm_wrapper.png
+code2flow ~/GitHub/PredictiveEcology/libcbmr/inst/python/libcbm/libcbm/input --language py --output inst/python/libcbm_call_graphs/libcbm_input.png
+code2flow ~/GitHub/PredictiveEcology/libcbmr/inst/python/libcbm/libcbm/model --language py --output inst/python/libcbm_call_graphs/libcbm_model.png
+code2flow ~/GitHub/PredictiveEcology/libcbmr/inst/python/libcbm/libcbm/model/cbm --language py --output inst/python/libcbm_call_graphs/libcbm_model_cbm.png
+code2flow ~/GitHub/PredictiveEcology/libcbmr/inst/python/libcbm/libcbm/model/cbm_exn --language py --output inst/python/libcbm_call_graphs/libcbm_model_cbm_exn.png
+code2flow ~/GitHub/PredictiveEcology/libcbmr/inst/python/libcbm/libcbm/model/model_definition --language py --output inst/python/libcbm_call_graphs/libcbm_model_model_definition.png
+code2flow ~/GitHub/PredictiveEcology/libcbmr/inst/python/libcbm/libcbm/model/moss_c --language py --output inst/python/libcbm_call_graphs/libcbm_model_moss_c.png
+code2flow ~/GitHub/PredictiveEcology/libcbmr/inst/python/libcbm/libcbm/storage --language py --output inst/python/libcbm_call_graphs/libcbm_storage.png
+#code2flow ~/GitHub/PredictiveEcology/libcbmr/inst/python/libcbm/libcbm/wrapper --language py --output inst/python/libcbm_call_graphs/libcbm_wrapper.png
