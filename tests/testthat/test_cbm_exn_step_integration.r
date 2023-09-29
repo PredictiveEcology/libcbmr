30a473ed7d542828543b6c98e5958a600a5b7ec0
Scott Morken <scottm361@gmail.com>
2023-09-28 20:16:36 -0700

    set reasonable initial values for cbm_vars so that the model runs

--- tests/testthat/test_cbm_exn_step_integration.r
+++ tests/testthat/test_cbm_exn_step_integration.r
@@ -20,7 +20,6 @@ test_that(
     flux_names <- lapply(flux, function(x) x["name"])
     n_stands <- 2L
 
-
     cbm_exn_parameters <- dict(
       slow_mixing_rate = read.csv(
         file.path(param_path, "slow_mixing_rate.csv")
@@ -51,6 +50,26 @@ test_that(
     # convert to the dict[str: pd.DataFrame] format expected by cbm_exn
     # step by default
     cbm_vars <- cbm_vars$to_pandas()
+    # set some reasonable values
+    cbm_vars$pools[, ] <- 1.0
+    cbm_vars$flux[, ] <- 0.0
+
+    cbm_vars$parameters[, "mean_annual_temperature"] <- -1.0
+    cbm_vars$parameters[, "disturbance_type"] <- 0L
+    cbm_vars$parameters[, "merch_inc"] <- 0.1
+    cbm_vars$parameters[, "foliage_inc"] <- 0.01
+    cbm_vars$parameters[, "other_inc"] <- 0.05
+
+    cbm_vars$state[, "area"] <- 1.0
+    cbm_vars$state[, "spatial_unit_id"] <- 3L
+    cbm_vars$state[, "land_class_id"] <- 0L
+    cbm_vars$state[, "age"] <- 100L
+    cbm_vars$state[, "species"] <- 6L
+    cbm_vars$state[, "sw_hw"] <- 0L
+    cbm_vars$state[, "time_since_last_disturbance"] <- 0L
+    cbm_vars$state[, "time_since_land_use_change"] <- 0L
+    cbm_vars$state[, "last_disturbance_type"] <- 0L
+    cbm_vars$state[, "enabled"] <- 1L
 
     testthat::expect_equal(nrow(cbm_vars$pools), n_stands)
     cbm_vars <- cbm_exn$cbm_exn_step(cbm_vars, cbm_exn_parameters)
